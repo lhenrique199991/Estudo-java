7bc887a001ede1dba2a704d87fceae95494ded5a
LuizzDev <lhso199991@gmail.com>
2024-08-26 23:48:15 -0300

    a

--- ControFluxo/src/ResultadoEscolar.java
+++ ControFluxo/src/ResultadoEscolar.java
@@ -1,6 +1,6 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 2;
+        int nota = 6;
 
         //Estrutura de fluxo em encadeada
         if(nota >= 7)
@@ -15,6 +15,14 @@ public class ResultadoEscolar {
 
         else
             System.out.println("Reprovado");
+
+        
+        //Condição ternária
+        String resultado = nota >=7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+
+        System.out.println(resultado);
+
+
     }
     
 }
