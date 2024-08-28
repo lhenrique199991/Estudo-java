fd539ba6963f1aaa4c216b9d6d6ab72c31ef2417
LuizzDev <lhso199991@gmail.com>
2024-08-27 23:46:43 -0300

    a

---
+++ ControFluxo/src/ExemploFoArrey.java
@@ -0,0 +1,15 @@
+public class ExemploFoArrey {
+    public static void main(String[] args) {
+
+        String alunos [] = {"felipe", "luiz", "caio", "carlos"};
+
+        //for (int x = 0 ; x < alunos.length; x++ ){
+
+        for (String aluno: alunos){
+            System.out.println("O nome do aluno é: " + aluno);
+        }
+
+            //System.out.println("O aluno no indice x = " + x + " é " + alunos[x]);
+        
+    }
+}
